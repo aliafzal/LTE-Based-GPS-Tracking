3c91c2a6783b773ba90e9d8dd7593840a86680d0
aliafzal <aliafzal9323@gmail.com>
2018-08-07 13:15:32 -0300

    Initial Commit

--- PVT_app.h
+++ PVT_app.h
@@ -1,13 +1,6 @@
-/*********************************************************************************************
-   FILE:          PVT_app.h
-----------------------------------------------------------------------------------------------
-   COPYRIGHT:     (c) 2016 STMicroelectronics
-**********************************************************************************************/
-
 #include <gpOS.h>
 
 struct atcChannel;
 
 extern void pvt_app_init(const struct atcChannel *atc);
-//extern void pvt_app_init(gpOS_partition_t *part, const struct atcChannel *atc);
 
